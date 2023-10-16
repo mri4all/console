f9c06317fa4649b548577e26972eb677e7fa263c
hsun981 <heng.sun@yale.edu>
2023-10-16 17:43:56 +0000

    draft for traj visualization and testing

---
+++ sequences/common/view_traj.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+import math
+import numpy as np
+from matplotlib import pyplot
+
+from PyQt5 import uic
+
+import pypulseq as pp  # type: ignore
+
+def view_traj(sequence_instance=None) -> bool:
+    [k_traj_adc, k_traj, t_excitation, t_refocusing, t_adc] = sequence_instance.calculate_kspace()
+
+    pyplot.figure()
+    pyplot.plot(t_adc,k_traj_adc)
+
+    return True
