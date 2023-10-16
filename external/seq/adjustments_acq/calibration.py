4555b22203df7a4f3e6b252553da6a0aba874dfe
Anais-A <anais.artiges@free.fr>
2023-10-16 19:42:56 +0000

    Fininding the frequency corresponding to the max snr

--- external/seq/adjustments_acq/calibration.py
+++ external/seq/adjustments_acq/calibration.py
@@ -55,6 +55,7 @@ def larmor_step_search(seq_file=constants.DATA_PATH_ACQ/'se_6.seq', step_search_
     time.sleep(delay_s)
 
     # Repeat for each frequency after the first
+    snr_array = []
     for i in range(1, steps):
         print(f'{swept_freqs[i]:.4f} MHz ({i}/{steps})')
         rx_arr[:, i], _ = scr.run_pulseq(seq_file, rf_center=swept_freqs[i],
@@ -65,6 +66,7 @@ def larmor_step_search(seq_file=constants.DATA_PATH_ACQ/'se_6.seq', step_search_
         # Calculate signal to noise ratio 
         snr = np.mean(np.abs(rx_arr[:,i])) / np.abs(np.std(rx_arr[:,i]))
         print("SNR = " + str(snr))
+        snr_array.append(snr)
         time.sleep(delay_s)
 
     # Find the frequency data with the largest maximum absolute value
@@ -72,7 +74,11 @@ def larmor_step_search(seq_file=constants.DATA_PATH_ACQ/'se_6.seq', step_search_
     max_freq = swept_freqs[max_ind]
     print(f'Max frequency: {max_freq:.4f} MHz')
 
-    
+    # Find the frequency data with the largest maximum absolute value
+    max_snr_ind = np.argmax(np.max(np.abs(snr_array), axis=0, keepdims=False))
+    max_snr_freq = swept_freqs[max_snr_ind]
+    print(f'Max SNR frequency: {max_snr_freq:.4f} MHz')
+
 
     # Plot figure
     if plot:
