70f0a705e43a2a495b394977fbadbb65a6239ab0
Tarun Dutt <Tarun.Dutt@nyulangone.org>
2023-10-17 22:11:43 +0000

    add utility to reorganize exam data in StudyViewer()

--- services/ui/studyviewer.py
+++ services/ui/studyviewer.py
@@ -1,11 +1,17 @@
 import json
 import numpy as np
 from pathlib import Path
+from pydantic import BaseModel
 from PyQt5 import uic
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *  # type: ignore
+from typing import List, Dict
 
+from common.task import read_task
+from common.types import (
+    ScanTask,
+)
 from common.version import mri4all_version
 import common.runtime as rt
 import common.logger as logger
@@ -16,48 +22,75 @@ def show_viewer():
     w = StudyViewer()
     w.exec_()
 
+class ScanData(BaseModel):
+    dicom: List[float]
+    rawdata: List[float]
+    metadata: ScanTask
+
+class ExamData(BaseModel):
+    id: str
+    scans: List[ScanData]
+
+class PatientData(BaseModel):
+    id: str
+    exams: List[ExamData]
+
+class OrganizedData(BaseModel):
+    patients: List[PatientData]
+
 
 class StudyViewer(QDialog):
-    patient_info = {"31431":{"name":"Patient A", "exams": [{"id":"ABCD"}]},"43213":{"name":"Patient B", "exams": [{"id":"DEFG"}, {"id":"SDFSDF"}]}}
-
     def __init__(self):
         super(StudyViewer, self).__init__()
         uic.loadUi(f"{rt.get_console_path()}/services/ui/forms/studyviewer.ui", self)
         self.setWindowTitle("Study Viewer")
 
-        self.patientComboBox.addItems([p["name"] for p in self.patient_info.values()])
+        self.archive_path = Path('/opt/mri4all/data/archive')
+        self.organized_data = self.organize_scan_data_from_folders()
+
+        self.patientComboBox.addItems([p.id for p in self.organized_data.patients])
         self.patientComboBox.currentIndexChanged.connect(self.patient_selected)
-
-        self.archived_exam_path = Path('/opt/mri4all/data/archive')
-
-        self.archived_exams_data = [self.get_exam_data(i) for i in self.archived_exam_path.glob('*') if i.is_dir()]
-
-        print(self.archived_exams_data)
 
         self.patient_selected(0)
 
     def patient_selected(self, index):
         self.examListWidget.clear()
-        the_patient = self.patient_info[list(self.patient_info.keys())[index]]
-        for exam in the_patient["exams"]:
-            self.examListWidget.addItem(exam["id"])
+        the_patient = self.organized_data.patients[index]
+        for exam_obj in the_patient.exams:
+            self.examListWidget.addItem(exam_obj.id)
         pass
 
-    
-    def get_exam_data(self, path):
-        data = {}
-        data['dicom'] = np.array([]) # read dicom data from Dicom folder
-        data['rawdata'] = np.array([]) # read raw data from rawdata folder
+    def organize_scan_data_from_folders(self):
+        organized_data = OrganizedData(patients=[])
+        for exam_dir in self.archive_path.iterdir():
+            if not exam_dir.is_dir():
+                continue
             
-        metadata_file = Path(path) / 'scan.json'
-        if metadata_file.is_file():
-            with open(metadata_file, 'r') as f:
-                data['scan_metadata'] = json.load(f)
-        else:
-            raise FileNotFoundError(f"scan.json not found in : {path}")
-        
-        return data
+            exam_id, scan_num = str(exam_dir.name).split('#')
+            scan_task = read_task(exam_dir)
+            patient_id = f"{scan_task.patient.last_name}, {scan_task.patient.first_name}"
+            #exam_id = scan_task["exam"]["id"] # should be same as id found in dir_name
+            scan_id = scan_task.id
 
+            # create a new patient object if not found
+            patient = next((p for p in organized_data.patients if p.id == patient_id), None)
+            if not patient:
+                patient = PatientData(id=patient_id, exams=[])
+                organized_data.patients.append(patient)
+            
+            # create a new exam object if not found
+            exam = next((e for e in patient.exams if e.id == exam_id), None)
+            if not exam:
+                exam = ExamData(id=exam_id, scans=[])
+                patient.exams.append(exam)
+
+            dicom_data = np.array([])
+            rawdata = np.array([])
+
+            scan = ScanData(dicom=dicom_data, rawdata=rawdata, metadata=scan_task)
+            exam.scans.append(scan)
+
+        return organized_data
 
     def close_clicked(self):
         self.close()
