e8a238148d7317751b8cbe34c8a0b99190a3175e
Anais-A <anais.artiges@free.fr>
2023-10-17 23:41:58 +0000

    Provided neat reader and writer for the json config file

--- external/seq/adjustments_acq/test_write_json.py
+++ external/seq/adjustments_acq/test_write_json.py
@@ -1,3 +1,4 @@
-from util import update_json_parameter
+from util import reading_json_parameter, writing_json_parameter
 
-update_json_parameter()
+configuration_data=reading_json_parameter(file_name='config.json')
+writing_json_parameter(config_data=configuration_data, file_name='config.json')
