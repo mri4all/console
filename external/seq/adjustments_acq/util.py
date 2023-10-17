110097d16ead2a3b9e2a60bea9ff1bdf142b906b
Anais-A <anais.artiges@free.fr>
2023-10-17 23:24:11 +0000

    Reading and writing in json config file

--- external/seq/adjustments_acq/util.py
+++ external/seq/adjustments_acq/util.py
@@ -2,16 +2,17 @@ import json
 from pydanticConfig import Config
 
 def update_json_parameter():
-    with open('config_test.json', 'r+') as file:
+    with open('config.json') as file:
         data = json.load(file)
         configuration_data = Config(**data)
+
+    print(configuration_data)
         
-        ### writing ###
-        file.seek(0)
-        indent_size = 4
-        json.dumps(configuration_data.model_dump(mode="json"),
-                   file,
-                   indent=indent_size)
+    ### writing ###
+    #file.seek(0)
+    indent_size = 4
+    with open('config_test.json', 'w') as outFile:
+        outFile.write(json.dumps(configuration_data.model_dump(mode="json"), indent=indent_size))
             
         
         
