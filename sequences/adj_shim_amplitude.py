f8cc6a1ac20031383e7a4ca4a93600314d739472
jonbmartin <jon.bach.martin@gmail.com>
2023-10-17 17:43:44 +0000

    update shim cal overall structure

--- sequences/adj_shim_amplitude.py
+++ sequences/adj_shim_amplitude.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-#from external.seq.adjustments_acq.calibration import grad_max_cal
+from external.seq.adjustments_acq.calibration import shim_cal
 import external.seq.adjustments_acq.config as cfg
 
 import common.logger as logger
@@ -36,7 +36,22 @@ class CalShimAmplitude(PulseqSequence, registry_key=Path(__file__).stem):
     def run_sequence(self) -> bool:
         log.info("Running sequence " + self.get_name())
 
-        #TODO: unimplemented! Insert code here
-
+        shim_cal(larmor_freq=cfg.LARMOR_FREQ,
+                channel='x',
+                range=0.01,
+                shim_points=3,
+                points=2,
+                iterations=1,
+                zoom_factor=2,
+                shim_x=cfg.SHIM_X,
+                shim_y=cfg.SHIM_Y,
+                shim_z=cfg.SHIM_Z,
+                tr_spacing=2,
+                force_tr=False,
+                first_max=False,
+                smooth=True,
+                plot=True,
+                gui_test=False)
+        
         log.info("Done running sequence " + self.get_name())
         return True
