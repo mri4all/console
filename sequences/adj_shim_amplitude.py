8bf2d4a980ab02df57e0b7e067f676546a15678a
jonbmartin <jon.bach.martin@gmail.com>
2023-10-20 14:25:57 +0000

    add plotting option for shimming

--- sequences/adj_shim_amplitude.py
+++ sequences/adj_shim_amplitude.py
@@ -6,7 +6,7 @@ import external.seq.adjustments_acq.config as cfg
 import common.logger as logger
 
 from sequences import PulseqSequence
-from sequences.rf_se import pypulseq_rfse  # type: ignore
+from sequences.rf_se import pypulseq_rfse
 
 import configparser
 
@@ -23,7 +23,7 @@ class CalShimAmplitude(PulseqSequence, registry_key=Path(__file__).stem):
     def get_description(self) -> str:
         return "Adjust Shim Sequence."
 
-    def calculate_sequence(self, scan_task) -> bool:
+    def calculate_sequence(self) -> bool:
         self.seq_file_path = self.get_working_folder() + "/seq/shim.seq"
         log.info("Calculating sequence " + self.get_name())
 
@@ -33,27 +33,26 @@ class CalShimAmplitude(PulseqSequence, registry_key=Path(__file__).stem):
         self.calculated = True
         return True
 
-    def run_sequence(self, scan_task) -> bool:
+    def run_sequence(self) -> bool:
         log.info("Running sequence " + self.get_name())
 
-        shim_cal(
-            larmor_freq=cfg.LARMOR_FREQ,
-            channel="x",
-            range=0.01,
-            shim_points=3,
-            points=2,
-            iterations=1,
-            zoom_factor=2,
-            shim_x=cfg.SHIM_X,
-            shim_y=cfg.SHIM_Y,
-            shim_z=cfg.SHIM_Z,
-            tr_spacing=2,
-            force_tr=False,
-            first_max=False,
-            smooth=True,
-            plot=True,
-            gui_test=False,
-        )
-
+        shim_cal(seq_file=self.seq_file_path,
+                larmor_freq=cfg.LARMOR_FREQ,
+                channel='x',
+                range=0.01,
+                shim_points=3,
+                points=2,
+                iterations=1,
+                zoom_factor=2,
+                shim_x=cfg.SHIM_X,
+                shim_y=cfg.SHIM_Y,
+                shim_z=cfg.SHIM_Z,
+                tr_spacing=2,
+                force_tr=False,
+                first_max=False,
+                smooth=True,
+                plot=True,
+                gui_test=False)
+        
         log.info("Done running sequence " + self.get_name())
         return True
