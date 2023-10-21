4219cec414e5b2faea256e6b0f1c9351cbbd695a
vagrant <jjemmachen@gmail.com>
2023-10-21 01:52:58 +0000

    recon update

---
+++ recon/gradient_delay.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def grad_delay_correction(kData, delayT, ETL, BW, )
+    pass
+
+# for l in range(L + 1):
+#     phi = 2 * np.pi * f_L[l] * T
+#     kspace_L = rawData * np.exp(1j * phi)
