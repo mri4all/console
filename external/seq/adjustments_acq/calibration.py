Corrected SNR calculation

external/seq/adjustments_acq/calibration.py
@@ -55,7 +55,7 @@ def larmor_step_search(seq_file=constants.DATA_PATH_ACQ/'se_6.seq', step_search_
     rx_arr = np.zeros((rxd.shape[0], steps), dtype=np.cdouble)
     rx_arr[:, 0] = rxd
     noise_array = np.zeros((int(rxd.shape[0]/2), steps), dtype=np.cdouble)
-    signal_array = np.zeros((int(rxd.shape[0]/2)+1, steps), dtype=np.cdouble)
+    signal_array = np.zeros((int(rxd.shape[0]/2), steps), dtype=np.cdouble)
 
     # Pause for spin recovery
     time.sleep(delay_s)
@@ -72,7 +72,7 @@ def larmor_step_search(seq_file=constants.DATA_PATH_ACQ/'se_6.seq', step_search_
         
         # Calculate signal to noise ratio
         for index in range(0,len(rx_arr[:, i])):
-            if index >= len(rx_arr[:, i])/4 and index < len(rx_arr[:, i]) - len(rx_arr[:, i])/4:
+            if index >= len(rx_arr[:, i])/4 and index < (len(rx_arr[:, i]) - len(rx_arr[:, i])/4):
                 signal_array[index,i] = rx_arr[index, i]
             else:
                 noise_array[index,i] = rx_arr[index, i]