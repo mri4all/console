f58d501b366f81243e3987cc16289d919d8b9099
hsun981 <heng.sun@yale.edu>
2023-10-21 21:11:20 +0000

    change function name

--- sequences/se_1D.py
+++ sequences/se_1D.py
@@ -147,7 +147,7 @@ class SequenceRF_SE(PulseqSequence, registry_key=Path(__file__).stem):
             # plt.plot(np.abs(recon))
             # plt.title("fft signal")
             # plt.show()
-            view_traj.view_sig(rxd, self.get_working_folder())
+            view_traj.view_sig_1d(rxd, self.get_working_folder())
 
 
         log.info("Done running sequence " + self.get_name())
