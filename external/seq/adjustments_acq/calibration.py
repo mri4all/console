4f9110dea285873f327d5b83a4478c2538013335
Anais-A <anais.artiges@free.fr>
2023-10-21 16:40:51 +0000

    Implementing UI for adj_frequency

--- external/seq/adjustments_acq/calibration.py
+++ external/seq/adjustments_acq/calibration.py
@@ -61,24 +61,24 @@ def larmor_step_search(seq_file=constants.DATA_PATH_ACQ/'se_6.seq', step_search_
     time.sleep(delay_s)
 
     # Repeat for each frequency after the first
-    snr_array = []
-    for i in range(1, steps):
-        print(f'{swept_freqs[i]:.4f} MHz ({i}/{steps})')
-        rx_arr[:, i], _ = scr.run_pulseq(seq_file, rf_center=swept_freqs[i],
-                                         tx_t=1, grad_t=10, tx_warmup=100,
-                                         shim_x=shim_x, shim_y=shim_y, shim_z=shim_z,
-                                         grad_cal=False, save_np=False, save_mat=False, save_msgs=False,
-                                         gui_test=gui_test)
+    # snr_array = []
+    # for i in range(1, steps):
+    #     print(f'{swept_freqs[i]:.4f} MHz ({i}/{steps})')
+    #     rx_arr[:, i], _ = scr.run_pulseq(seq_file, rf_center=swept_freqs[i],
+    #                                      tx_t=1, grad_t=10, tx_warmup=100,
+    #                                      shim_x=shim_x, shim_y=shim_y, shim_z=shim_z,
+    #                                      grad_cal=False, save_np=False, save_mat=False, save_msgs=False,
+    #                                      gui_test=gui_test)
         
-        # Calculate signal to noise ratio
-        for index in range(0,rxd.shape[0]):
-            if index > rxd.shape[0]/4 and index < (rxd.shape[0] - rxd.shape[0]/4):
-                signal_array[index,i] = rx_arr[index, i]
-            else:
-                noise_array[index,i] = rx_arr[index, i]
-        snr = np.mean(np.abs(signal_array[:,i])) / np.std(np.abs(noise_array[:,i]))
-        print("SNR= " + str(snr))
-        snr_array.append(snr)
+    #     # Calculate signal to noise ratio
+    #     for index in range(0,rxd.shape[0]):
+    #         if index > rxd.shape[0]/4 and index < (rxd.shape[0] - rxd.shape[0]/4):
+    #             signal_array[index,i] = rx_arr[index, i]
+    #         else:
+    #             noise_array[index,i] = rx_arr[index, i]
+    #     snr = np.mean(np.abs(signal_array[:,i])) / np.std(np.abs(noise_array[:,i]))
+    #     print("SNR= " + str(snr))
+    #     snr_array.append(snr)
 
     # Find the frequency data with the largest maximum absolute value
     max_ind = np.argmax(np.max(np.abs(rx_arr), axis=0, keepdims=False))
@@ -86,9 +86,9 @@ def larmor_step_search(seq_file=constants.DATA_PATH_ACQ/'se_6.seq', step_search_
     print(f'Max frequency: {max_freq:.4f} MHz')
 
     # Find the frequency data with the largest maximum SNR value
-    max_snr_ind = np.argmax(snr_array)
-    max_snr_freq = swept_freqs[max_snr_ind]
-    print(f'Max SNR frequency: {max_snr_freq:.4f} MHz')
+    # max_snr_ind = np.argmax(snr_array)
+    # max_snr_freq = swept_freqs[max_snr_ind]
+    # print(f'Max SNR frequency: {max_snr_freq:.4f} MHz')
 
     # Plot figure
     if plot:
@@ -102,14 +102,14 @@ def larmor_step_search(seq_file=constants.DATA_PATH_ACQ/'se_6.seq', step_search_
         plt.show()
 
     # Plot noise figure
-    if plot:
-        fig, axs = plt.subplots(2, 1, constrained_layout=True)
-        fig.suptitle('NOISE')
-        axs[0].plot(np.abs(signal_array))
-        axs[0].set_title('signal_array')
-        axs[1].plot(np.abs(noise_array))
-        axs[1].set_title('noise_array')
-        plt.show()
+    # if plot:
+    #     fig, axs = plt.subplots(2, 1, constrained_layout=True)
+    #     fig.suptitle('NOISE')
+    #     axs[0].plot(np.abs(signal_array))
+    #     axs[0].set_title('signal_array')
+    #     axs[1].plot(np.abs(noise_array))
+    #     axs[1].set_title('noise_array')
+    #     plt.show()
 
     # Output of useful data for visualization
     data_dict = {'rx_arr': rx_arr,
