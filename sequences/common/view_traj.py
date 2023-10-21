8a82573c3b991ee0fa1cde8badd855cd140ffcc3
hsun981 <heng.sun@yale.edu>
2023-10-21 00:38:36 +0000

    dark background for traj

--- sequences/common/view_traj.py
+++ sequences/common/view_traj.py
@@ -9,14 +9,26 @@ from pypulseq.Sequence import parula
 from PyQt5 import uic
 
 def view_traj_2d(k_traj_adc, k_traj):
-    plt.figure()
-    plt.plot(k_traj[0,:], k_traj[1,:],'b',linewidth=1)
-    plt.plot(k_traj_adc[0,:], k_traj_adc[1,:], 'r.',markersize=0.5)
-    plt.axis('equal')
-    plt.xlabel('kx')
-    plt.ylabel('ky')
-    plt.title('K-space Trajactory')
+    plt.style.use("dark_background")
+    # fig, ax = plt.subplots()
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.plot(k_traj[0,:], k_traj[1,:],linewidth=1)
+    ax.plot(k_traj_adc[0,:], k_traj_adc[1,:], 'm.',markersize=0.5)
+    ax.set_aspect('equal')
+    ax.set_xlabel('kx')
+    ax.set_ylabel('ky')
+    ax.set_title('K-space Trajactory')
     plt.show()  # Display the plots
+    # plt.figure()
+    # plt.style.use("dark_background")
+    # plt.plot(k_traj[0,:], k_traj[1,:],color='b',linewidth=1)
+    # plt.plot(k_traj_adc[0,:], k_traj_adc[1,:], color ='r',marker='.',markersize=0.5)
+    # plt.axis('equal')
+    # plt.xlabel('kx')
+    # plt.ylabel('ky')
+    # plt.title('K-space Trajactory')
+    # plt.show()  # Display the plots
 
 def view_traj_3d(k_traj_adc, k_traj):
     fig = plt.figure()
