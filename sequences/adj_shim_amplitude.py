3ad30b0a9f72c29ce35fdddf23a4ec9080a2723f
jonbmartin <jon.bach.martin@gmail.com>
2023-10-21 16:45:19 +0000

    specify shim iter locally . TODO add UI option

--- sequences/adj_shim_amplitude.py
+++ sequences/adj_shim_amplitude.py
@@ -102,7 +102,7 @@ class CalShimAmplitude(PulseqSequence, registry_key=Path(__file__).stem):
         log.info("Running sequence " + self.get_name())
         
         shim_range = 0.1
-        
+        n_iter_linear = 2
         for shim_iter in range(int(n_iter_linear)):
             
             for channel in axes:
