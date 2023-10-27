f902f487d9937a679af87226f0f6d2d3346e9eeb
Tobias Block <tobias.block@nyumc.org>
2023-10-27 03:10:19 +0000

    Path fix

--- external/marcos_experiments/my_first_experiment.py
+++ external/marcos_experiments/my_first_experiment.py
@@ -1,25 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import marcos_experiments.external  # imports external.py
-import marcos_client.experiment as ex  #
+
+import external.marcos_client.experiment as ex  #
 
 
 def my_first_experiment():
     exp = ex.Experiment(lo_freq=5, rx_t=3.125)
 
     event_dict = {
-        "tx0": (np.array([50, 130, 200, 360]), np.array([0.5, 0, 0.5j, 0])),    # tx0_i, tx0_q:: -1 to 1; -j to j
+        "tx0": (
+            np.array([50, 130, 200, 360]),
+            np.array([0.5, 0, 0.5j, 0]),
+        ),  # tx0_i, tx0_q:: -1 to 1; -j to j
         # "tx1": (np.array([500, 700]), np.array([0.2, 0])),
         # "grad_vx": (np.array([200, 700]), np.array([0.2, 0])),
         # "grad_vy": (np.array([300, 700]), np.array([0.2, 0])),
         # "grad_vz": (np.array([500, 700]), np.array([0.2, 0])),
-        "rx0_en": (np.array([400, 800]), np.array([1, 0])),     # adc 0
-        "rx1_en": (np.array([400, 800]), np.array([1, 0])),     # adc 1 - gradx
+        "rx0_en": (np.array([400, 800]), np.array([1, 0])),  # adc 0
+        "rx1_en": (np.array([400, 800]), np.array([1, 0])),  # adc 1 - gradx
     }
     exp.add_flodict(event_dict)
     exp.plot_sequence()
 
-    rxd, msgs = exp.run()       # rxd is the array
+    rxd, msgs = exp.run()  # rxd is the array
     exp.close_server(only_if_sim=True)
 
     plt.figure()
